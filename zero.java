74ca034612a3ac83e0591f6a3be90c11a1698b80
Shai Beekman <shaibeekman@gmail.com>
2024-07-08 01:41:47 -0400

    started development on zero.java
     named zero because it is the root or main file...this is more intuitive for branching
    
    obtained regexes for parsing :=choicer=: := {String string1} * {String string2}; created method for choicer as well

--- zero.java
+++ zero.java
@@ -1,4 +1,4 @@
-public class zero {
+public class zero {//https://docs.google.com/document/d/12fkveaWyaNZDhDMPH_Sil3t3QibOxJ7GbqX-jlTnlJo/edit
 
 //    So let’s imagine that N is a logical system.
 //
@@ -11,4 +11,39 @@ public class zero {
 //    {String string}*{String string} := implement either String
 //    {!String string}*{String string} := &!String string& more easy to interpret than &String string&;
 //    implement either String
+public static void main(String[] args) {
+
+    if String.equals("{String string}*{String string}");
+    String string1 = String, String string1 = String;
+
+    ^(.*?(\b{String string}*{String string}\b)[^$]*)$ regex;
+        //https://stackoverflow.com/questions/9348326/regex-find-word-in-the-string
+    "\w*",\s+"[\w\s]*";
+        //https://stackoverflow.com/questions/12572353/arbitrary-length-string-space-regular-expression
+
+//how to improve regexes?
 }
+
+public mathod 1 () {
+        if String.equals("{String string}*{String string}");
+        String string1 = String, String string1 = String;
+
+    ^(.*?(\b{String string}*{String string}\b)[^$]*)$ regex;
+        //https://stackoverflow.com/questions/9348326/regex-find-word-in-the-string
+        "\w*",\s+"[\w\s]*";
+        //https://stackoverflow.com/questions/12572353/arbitrary-length-string-space-regular-expression
+
+//how to improve regexes?
+    }
+    public mart 2 () {
+if String.equals("{String string}*{String string}");
+    String string1 = String, String string1 = String;
+
+    ^(.*?(\b{String string}*{String string}\b)[^$]*)$ regex;
+    //https://stackoverflow.com/questions/9348326/regex-find-word-in-the-string
+    "\w*",\s+"[\w\s]*";
+    //https://stackoverflow.com/questions/12572353/arbitrary-length-string-space-regular-expression
+
+//how to improve regexes?
+}}
+
