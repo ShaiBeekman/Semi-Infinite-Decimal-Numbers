80521528fc211d1010667271e929c2d3b0446b43
Shai Beekman <shaibeekman@gmail.com>
2024-07-07 23:03:50 -0400

    added zero.java

---
+++ zero.java
@@ -0,0 +1,14 @@
+public class zero {
+
+//    So let’s imagine that N is a logical system.
+//
+//    That logical system can have many components.
+//    Or just a system||<;...:
+//    Since that system is infinite, it can have many components.
+//
+//    And since it is attached to a finite cardinal  , it ccan attribute to that cardinal which is finite in nature its {!components}*{instricnic nature}, so that the infinite cardinal gives its properties to the finite {!cardinal}*{counterpart}
+//
+//    {String string}*{String string} := implement either String
+//    {!String string}*{String string} := &!String string& more easy to interpret than &String string&;
+//    implement either String
+}
